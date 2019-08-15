a907c5f3b51820aad5ca2e2b91ac90c127c5cbb2
lizhigaomac <lizhigao@021.com>
2019-08-15 18:23:32 +0800

    update

---
+++ src/app.js
@@ -0,0 +1,30 @@
+require('module-alias/register');
+
+const Koa = require('koa');
+const app = new Koa();
+
+// logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(async ctx => {
+  // console.log(ctx);
+  ctx.body = 'Hello World!';
+});
+
+// app.on('error', (err, ctx) => {
+//   console.error('server error', err, JSON.stringify(ctx));
+// });
+
+app.listen(3000);
